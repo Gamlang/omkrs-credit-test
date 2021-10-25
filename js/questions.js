940570dc2f3bea03146bdab7cfb3ef1b8ed02e30
Gamlang <80462045+Gamlang@users.noreply.github.com>
2021-10-25 19:15:24 +0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Siaoa Oenghianat Omkrs?",
+    answer: "Erlan",
+    options: [
+      "Darhan",
+      "Erlan",
+      "Xina",
+      "Esde"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Siaoa Teman Seoklah Elpij?",
+    answer: "Iky Banyuk",
+    options: [
+      "Iky Banyuk",
+      "Stevek (XT)",
+      "Galanj",
+      "Halim FF"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Aoakah Omek Memounyai Discrot?",
+    answer: "Btuo",
+    options: [
+      "Btul",
+      "Btuo",
+      "YNTKS",
+      "Tidack"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Aoakah Fiyan Esde?",
+    answer: "Btul",
+    options: [
+      "Btul",
+      "Esde",
+      "Esempe",
+      "YNTKTS"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Aoakah Galanj Oiatu?",
+    answer: "Btul Bnaget",
+    options: [
+      "Btul Bnaget",
+      "Btuo",
+      "Ndak",
+      "YNTKTS mulu kontol"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
